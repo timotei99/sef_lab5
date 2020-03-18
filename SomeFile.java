bbc4db9aeae850486afc4d4eb535d93a342bd5eb
Caracoancea Timotei <caracoancea.timotei@gmail.com>
2020-03-19 00:31:59 +0200

    Commit for ex11

---
+++ SomeFile.java
@@ -0,0 +1,5 @@
+class SomeFile{
+    private static void main(String[] args){
+        System.out.println("Here i am in branch BranchToWorkIn");
+    }
+}
--- lab1-master/src/loose/oose/fis/Main.java
+++ lab1-master/src/loose/oose/fis/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //This is the line i modified in the first branch, the comment itself is the modification
         String[] xmlList = new String[6];
         xmlList[0] = "<tag1>";
         xmlList[1] = "text1";
